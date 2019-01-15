870b60d382e28a9cf486a8d0e46240ecf0886939
Gan Dong <hotterd@gmail.com>
2019-01-15 19:17:34 +0800

    Initial commit

---
+++ src/main/java/info/donggan/demo/MavenProjectDemoApplication.java
@@ -0,0 +1,14 @@
+package info.donggan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjectDemoApplication.class, args);
+	}
+
+}
+
